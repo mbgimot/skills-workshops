c3bf988dd6af7f3eca0cd52e93064faddd3bd425
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-03 17:28:48 +0000

    first test and method

--- week-1/rectangle_kata/lib/rectangle.rb
+++ week-1/rectangle_kata/lib/rectangle.rb
@@ -1,3 +1,5 @@
 def rectangle
-  "THIS SHOULD BE A RECTANGLE!"
+  open_close_line = "|------------------|\n"
+  middle_lines = "|                  |\n"
+  "#{open_close_line}#{middle_lines * 8}#{open_close_line}"
 end
